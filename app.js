521dd2b0a5d49d4f6cf5e2351a8a2fcf2f7e6810
Daniel Redington <japaniel@japaniel.com>
2013-06-09 03:09:29 +0000

    F::send static files

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var restify = require('restify');
-var jade = reqire('jade');
+var jade = require('jade');
 
 var server = restify.createServer();
-server.get('/', function (req, res, next) {
-	res.send(200, 'Hey, this works');
+
+
+server.get('/', restify.serveStatic({directory: './public'}),function (req, res, next) {
+	fs.readFile('./index.html', function (err, file) {
+		if (err) {
+			res.send(500);
+			return next();
+		}
+
+		res.send({
+			code: 200,
+			noEnd: true,
+		});
+		res.send(file);
+		res.end();
+	});
+
 	return next();
 });
 
